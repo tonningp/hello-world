6586872dfd6e108dec23dc909be2cf5038971a25
Paul Tonning <paul.tonning@student.vvc.edu>
2018-09-15 09:58:38 -0700

    Added the cout statement.

--- hello.cpp
+++ hello.cpp
@@ -4,3 +4,15 @@ Description: a simple hello world program
 Author: Paul Tonning
 Email: paul.tonning@student.vvc.edu
 **/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "New Feature" << endl;
+
+
+   return 0;
+}
